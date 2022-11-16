41404a4b76900ce1b629ceb862ed896e2a9beab1
adcerro <adcerro@uninorte.edu.co>
2022-11-15 23:18:14 -0500

    fix: token now stored in a file

--- src/main/java/start/Pana.java
+++ src/main/java/start/Pana.java
@@ -8,7 +8,24 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.User;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.Buffer;
+
 public class Pana extends TelegramLongPollingBot {
+    private String code;
+
+    public Pana() {
+        try {
+            FileReader f = new FileReader("src/token.txt");
+            BufferedReader read = new BufferedReader(f);
+            code = read.readLine();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     @Override
     public String getBotUsername() {
@@ -17,32 +34,36 @@ public class Pana extends TelegramLongPollingBot {
 
     @Override
     public String getBotToken() {
-        return "5731229390:AAEo57wU-RUZsXjeBvIXjlQZws72sF_VUuQ";
+        return code;
     }
 
     @Override
     public void onUpdateReceived(Update update) {
         Message msg = update.getMessage();
         User user = msg.getFrom();
-        System.out.println(user.getFirstName()+" texted "+msg.getText());
+        System.out.println(user.getFirstName() + " texted " + msg.getText());
         copyMessage(user.getId(), msg.getMessageId());
     }
-    public void answer(Long dest,String text){
+
+    public void answer(Long dest, String text) {
         SendMessage.SendMessageBuilder smb = SendMessage.builder();
         smb.chatId(dest.toString());
         smb.text(text);
-        try{
+        try {
             execute(smb.build());
-        }catch (TelegramApiException e){};
+        } catch (TelegramApiException e) {
+        }
+        ;
     }
-    public void copyMessage(Long dest,Integer messageId){
+
+    public void copyMessage(Long dest, Integer messageId) {
         CopyMessage.CopyMessageBuilder cmb = CopyMessage.builder();
         cmb.fromChatId(dest);
         cmb.chatId(dest);
         cmb.messageId(messageId);
         try {
             execute(cmb.build());
-        }catch (TelegramApiException ex){
+        } catch (TelegramApiException ex) {
             throw new RuntimeException(ex);
         }
     }
