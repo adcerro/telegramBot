85d25482466fcc25d09629c512667c6e30672ca1
adcerro <adcerro@uninorte.edu.co>
2022-11-21 15:51:51 -0500

    bases for ploting

--- PanaBot.py
+++ PanaBot.py
@@ -1,11 +1,15 @@
-from telegram import Update
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 import telegram.ext as te
 import logging
 import os
+import numpy as np
 
+list = ['Vivo','Clase','Sexo','Edad','Tiquete','Costo']
+buttons = [[InlineKeyboardButton(a,callback_data=list.index(a))] for a in list]
+keys = InlineKeyboardMarkup(buttons)
 
 def start(update: Update, context: te.CallbackContext):
-    context.bot.send_message(chat_id=update.effective_chat.id, text=f"Saludos {update._effective_message.from_user.first_name}, para ver mis comandos usa /help")
+    context.bot.send_message(chat_id=update.effective_chat.id, text=f"Saludos {update._effective_message.from_user.first_name}, para ver mis comandos usa /ayuda")
 
 def help(update: Update, context: te.CallbackContext):
     helpMessage = open('helpMessage.txt', 'r').read()
@@ -13,9 +17,15 @@ def help(update: Update, context: te.CallbackContext):
 
 def variables(update: Update, context: te.CallbackContext):
     message='Lista de variables: \n'
-    list = ['vivo','clase','sexo','edad','tiquete','costo']
     for a in list : message= message+ a+'\n'
     context.bot.send_message(chat_id=update.effective_chat.id,text=message)
+
+def plotuni(update: Update, context: te.CallbackContext):
+    context.bot.send_message(chat_id=update.effective_chat.id,text='Seleccione variable:', reply_markup=keys)
+    
+
+def plotbi(update: Update, context: te.CallbackContext):
+    context.bot.send_message(chat_id=update.effective_chat.id,text='Seleccione variable:', reply_markup=keys)
 
 def online():
     mytoken = os.environ["TOKEN"]
@@ -29,10 +39,9 @@ def online():
 
 def main():
     #Cruasán icon by Icons8
-    #mytoken = open('token.txt','r').readline()
-    
-    #updater = te.Updater(mytoken) 
-    updater = online()
+    mytoken = open('token.txt','r').readline()
+    updater = te.Updater(mytoken) 
+    #updater = online()
 
     dispatcher = updater.dispatcher
 
@@ -42,8 +51,10 @@ def main():
     dispatcher.add_handler(te.CommandHandler('start', start))
     dispatcher.add_handler(te.CommandHandler(['ayuda','help'], help))
     dispatcher.add_handler(te.CommandHandler('variables', variables))
-    updater.idle()
-    #updater.start_polling()
+    dispatcher.add_handler(te.CommandHandler('plotunivariate', plotuni))
+    dispatcher.add_handler(te.CommandHandler('plotbivariate', plotbi))
+    #updater.idle()
+    updater.start_polling()
 
 
 if __name__ == '__main__':
